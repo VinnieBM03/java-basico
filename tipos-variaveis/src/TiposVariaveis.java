36af15bf860836905c8e412f04d2f348c7649306
VinnieBM03 <vbmagalhaes003@gmail.com>
2024-08-27 19:11:29 -0300

    tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        
+        byte menor = 100;
+        short pequeno = 32000;
+        int normal = 1250675; // mais comum
+        long longo = 1234567890987654321l; // precisa do l ou L no final
+        float flutuante = 1.23f; // precisa do f ou F no final
+        double duplo = 123.765; // mais preciso
+
+        final double PI = 3.14; // CAIXA ALTA e final antes do tipo
+
+        String meuNome = "Vinicius B M";
+
+        boolean verdade = true;
     }
 }
